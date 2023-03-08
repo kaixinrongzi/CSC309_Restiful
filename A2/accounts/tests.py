997a2355b878eb5d1373c52f8878520938726c9d
kaixinrongzi <snow.zhou@mail.utoronto.co>
2023-03-07 21:22:10 -0500

    a2

--- A2/accounts/tests.py
+++ A2/accounts/tests.py
@@ -1,3 +1,16 @@
+# from django.contrib.sites import requests
+from django.test.client import Client
+import requests
 from django.test import TestCase
 
+
 # Create your tests here.
+class Test(TestCase):
+    def test1(self):
+        url = f"http://localhost:8000/accounts/register/"
+        data = {"username": "", "password1": "", "password2": "", "email": "", "first_name": "", "last_name": ""}
+        self.client = Client()
+        res = self.client.post(url, data=data)
+        print("res:", res.text)
+        # self.assertEqual(res.text.count("This field is required"), 3)
+
