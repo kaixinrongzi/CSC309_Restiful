72144e0be57c0f1a94a4e69218220d223c52d1f4
zixiu meng <mzx@kaibai.local>
2023-03-16 15:01:12 -0400

    add

--- restify/hotels/views/hotel_view.py
+++ restify/hotels/views/hotel_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from django.core.exceptions import PermissionDenied
 # Create your views here.
 
 class AddHotel(CreateAPIView):
@@ -15,11 +16,19 @@ class SearchHotel(ListAPIView):
     serializer_class = HotelSerializer
 
     def get_query_set(self):
-
         Hotel.objects.filter()
+    pass
 
 class DeleteHotel(DestroyAPIView):
     permission_class = [IsAuthenticated]
     serializer_class = HotelSerializer
 
-    def 
+    def perform_destroy(self, instance):
+        if instance.owner != self.request.user:
+            raise PermissionDenied("You do not have permission to delete the property")
+
+        instance.delete()
+
+    def get_object(self):
+        hotel = get_object_or_404(Hotel.objects.all(), pk=self.kwargs['pk'])
+        return hotel
