d7f1e46136a369a2072f01360892329b0ffc6bb3
zixiu meng <mzx@kaibai.local>
2023-03-16 04:00:11 -0400

    change

--- restify/hotels/views/hotel_view.py
+++ restify/hotels/views/hotel_view.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class AddHotel(CreateAPIView):
-    pass
+    permission_classes = [IsAuthenticated]
+    serializer_class = HotelSerializer
 
-class AllHotel(ListAPIView):
-    pass
+# class AllHotel(ListAPIView):
+#     serializer_class = HotelSerializer
 
 # filter and order by
 class SearchHotel(ListAPIView):
-    pass
+    serializer_class = HotelSerializer
+
+    def get_query_set(self):
+
+        Hotel.objects.filter()
 
 class DeleteHotel(DestroyAPIView):
-    pass
+    permission_class = [IsAuthenticated]
+    serializer_class = HotelSerializer
+
+    def 
