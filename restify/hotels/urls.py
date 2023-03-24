e8cf10789b24971702705b0895deaebe22f60373
zixiu meng <mzx@kaibai.ht.home>
2023-03-24 01:05:36 -0400

    add url for reservation

--- restify/hotels/urls.py
+++ restify/hotels/urls.py
@@ -10,4 +10,11 @@ urlpatterns = [
     path('<int:pk>/delete/', views.DeleteHotel.as_view(), name='deletehotel'),
     path('search/', views.SearchHotel.as_view(), name='searchhotel'), 
     path('search/availability/', views.SearchHotelAvailability.as_view(), name='hotelavailability'),
+    path('reservation/list/', views.ReservationList.as_view(), name='reservationlist'), 
+    path('reservation/<int:pk>/approve/', views.ReservationApprove.as_view(), name='reservation_approve'), 
+    path('reservation/<int:pk>/deny/', views.ReservationDeny.as_view(), name='reservation_deny'), 
+    path('reservation/<int:pk>/terminate', views.ReservationTerminate.as_view(), name='reservation_terminate'), 
+    path('reservation/<int:pk>/cancel', views.ReservationCancel.as_view(), name='reservation_cancel'),
+    path('reservation/reserve', views.ReservationReserve.as_view(), name='reservation_create'), 
+    
 ]
--- restify/hotels/views/__init__.py
+++ restify/hotels/views/__init__.py
@@ -1,3 +1,4 @@
 # from .comment_view import AddComment
 from .hotel_view import AddHotel, AddAvailability, UpdateHotel, DeleteHotel, UpdateAvailability, SearchHotelAvailability, SearchHotel
+from .reservation_view import ReservationList, ReservationApprove, ReservationCancel, ReservationDeny, ReservationReserve, ReservationTerminate
 # from .reservation_view import AddReservation
