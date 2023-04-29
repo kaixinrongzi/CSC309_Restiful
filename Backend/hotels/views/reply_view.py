83ff1db73d9a798665b93ba89014ecd0e3484a83
kaixinrongzi <90364543+kaixinrongzi@users.noreply.github.com>
2023-04-29 08:06:00 -0400

    Update reply_view.py

--- Backend/hotels/views/reply_view.py
+++ Backend/hotels/views/reply_view.py
@@ -1,13 +1,15 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView, \
+    get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from django.http import QueryDict
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.contenttypes.models import ContentType
-from ..serializers import ReplySerializer
+from ..serializers import ReplySerializer, NotificationSerializer
 from ..models import Reply, Comment, Notification
 import sys
+
 sys.path.append("...")
 from accounts.models import MyUser
 
@@ -25,20 +27,20 @@ class ReplyAdd(CreateAPIView):
             author_id = reply['author']
             author = MyUser.objects.get(pk=author_id)
 
-        if 'detail' not in reply:
-            detail = ''
-        else:
-            detail = reply['detail']
-
-        if 'reply_to' not in reply:
+        if 'reply_to' not in reply or reply['reply_to'] == '':
             reply_to = ''
         else:
             reply_to = reply['reply_to']
 
-        if 'object_id' not in reply:
+        if 'object_id' not in reply or reply['object_id'] == '':
             object_id = ''
         else:
             object_id = reply['object_id']
+
+        if 'detail' not in reply or reply['detail'] == '':
+            detail = ''
+        else:
+            detail = reply['detail']
 
         ordinary_dict = {'reply_to': reply_to,
                          'object_id': object_id,
@@ -49,25 +51,60 @@ class ReplyAdd(CreateAPIView):
         serializer = self.get_serializer(data=ordinary_dict)
         serializer.is_valid(raise_exception=True)
         reply_obj = serializer.save()
-        reply_obj.save()
 
-        #find out who it is replying to
-        if reply_to == '11' or reply_to == 11:    # reply to a comment
+        # find out who it is replying to
+        if reply_to == '11' or reply_to == 11:  # reply to a comment
             try:
-                comment = Comment.objects.get(id=reply['object_id'])
+                comment = Comment.objects.get(id=object_id)
                 receiver = comment.author
-            except:
-                return Response({"error": "The comment you are replying to does not exist"}, status=status.HTTP_403_FORBIDDEN)
-        if reply_to == '12' or reply_to == 12:    # reply to a reply
+
+                # create notification
+                notif_data = {'receiver': receiver.id,
+                              'message': "a reply to your comment " + str(comment.id),
+                              'content_type': 12,
+                              'object_id': reply_obj.id}
+
+                notif_serializer = NotificationSerializer(data=notif_data)
+
+                if notif_serializer.is_valid(raise_exception=True):
+                    notif_serializer.save()
+                    receiver.get_notified()
+
+                else:
+                    return Response({"error": "no choice for content_type"}, status=status.HTTP_403_FORBIDDEN)
+            except Exception as e:
+                print(e)
+                return Response({"error": "The comment you are replying to does not exist"},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        if reply_to == '12' or reply_to == 12:  # reply to a reply
             try:
                 reply = Reply.objects.get(id=reply['object_id'])
                 receiver = reply.author
-            except:
-                return Response({"error": "The reply you are replying to does not exist"}, status=status.HTTP_403_FORBIDDEN)
+                # create notification
+                notif_data = {'receiver': receiver.id,
+                              'message': "a reply to your reply " + reply.id,
+                              'content_type': 12,
+                              'object_id': reply_obj.id}
 
-        # create notification
-        notif = Notification.objects.create(receiver=receiver,
-                                            message="reply to your comment or reply: " + str(reply['object_id']))
-        receiver.get_notified()
+                notif_serializer = NotificationSerializer(data=notif_data)
+                if notif_serializer.is_valid(raise_exception=True):
+                    notif_serializer.save()
+                    receiver.get_notified()
+                else:
+                    return Response({"error": "no choice for content_type"}, status=status.HTTP_403_FORBIDDEN)
+            except:
+                return Response({"error": "The reply you are replying to does not exist"},
+                                status=status.HTTP_403_FORBIDDEN)
+
 
         return Response({"sucess", "replied"}, status=status.HTTP_201_CREATED)
+
+
+class ReplyView(RetrieveAPIView):
+    serializer_class = ReplySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        reply = get_object_or_404(Reply, id=self.kwargs['pk'])
+        return reply
