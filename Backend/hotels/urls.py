79ea92e1b17f892969ab8c81b4e2934bba7006d1
zixiu meng <mzx@kaibai.local>
2023-04-30 02:00:58 -0400

    add

--- Backend/hotels/urls.py
+++ Backend/hotels/urls.py
@@ -1,25 +1,3 @@
-# from django.urls import include, path
-# # from . import views
-# from .views.hotel_view import AddHotel, AddAvailability, UpdateHotel, UpdateAvailability, DeleteHotel
-# from .views.comment_view import AddComment, GetComments, GetComment
-# from .views.reservation_view import AddReservation
-# from .views.notification_view import NotificationsViewAll
-#
-# # app_name = 'hotels'
-# urlpatterns = [
-#
-#     path('notification/view/', NotificationsViewAll.as_view(), name='viewnotification'),
-#     path('add/', AddHotel.as_view(), name="addhotel"),
-#     path('add/<int:pk>/availability/', AddAvailability.as_view(), name="addavailability"),
-#     path('<int:pk>/update/', UpdateHotel.as_view(), name="updatehotel"),
-#     path('/update/<int:pk>/availability/', UpdateAvailability.as_view(), name='updateavailability'),
-#     path('<int:pk>/delete/', DeleteHotel.as_view(), name='deletehotel'),
-#     path('comment/add/', AddComment.as_view(), name="addcomment"),
-#     path('reserve/', AddReservation.as_view(), name='reserve'),
-#     path('comments/view/', GetComments.as_view(), name='viewcomments'),
-#     path('comment/<int:pk>/view/', GetComment.as_view(), name='viewcomments'),
-# ]
-
 from django.urls import include, path
 from .views.hotel_view import *
 from .views.reservation_view import *
